258992a1b52622c0eb21af27e6ae739774fe7d8d
Upadhyay <gaganupadhyay.karan@gmail.com>
2026-02-08 10:29:36 +0530

    made few changes

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getMessaging } from "firebase-admin/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2M1uA3Pp0zHsTCx6w14c_DX1sYfjodg0",
-  authDomain: "expensetracker-2759d.firebaseapp.com",
-  projectId: "expensetracker-2759d",
-  storageBucket: "expensetracker-2759d.firebasestorage.app",
-  messagingSenderId: "874326601085",
-  appId: "1:874326601085:web:357c2be9efd52ea4bfb014",
-  measurementId: "G-CLSK6WETHM"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain:process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase

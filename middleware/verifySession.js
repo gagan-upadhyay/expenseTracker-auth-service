a3db34c64d58485856185eb97254297c87aa3c72
Upadhyay <gaganupadhyay.karan@gmail.com>
2026-03-23 22:10:06 +0530

    made few changes

--- middleware/verifySession.js
+++ middleware/verifySession.js
@@ -3,28 +3,6 @@ import { getRedisClient } from "../config/redisConnection.js";
 
 const redisClient = await getRedisClient();
 
-// export const verifySession = async(req, res, next)=>{
-//     try{
-//         console.log("From verify session");
-//         const token = req.cookies.accessToken;
-//         console.log("Value of token from verifySession of user service:", token);
-//         if(!token) return res.status(401).json({message:'Token missing'});
-//         const decoded = jwt.verify(token, process.env.SECRET);
-//         console.log("Value of decoded from auth verifySession:\n", decoded);
-
-//         const cachedToken = await redisClient.get(`session:${decoded.id}`);
-//         if(cachedToken!== token){
-//             return res.status(401).json({message:'Invalid or expired token'})
-//         }
-//         req.user=decoded;
-//         next();
-//     }catch(err){
-//         res.status(401).json({message:'Unauthorized!'});
-//     }
-// };
-
-
-
 export const verifySession = async(req, res, next)=>{
     try{
         if(process.env.NODE_ENV==='test'){
--- mocks/mockRedis.js
+++
@@ -1,3 +0,0 @@
-export default async function () {
-  await new Promise((r) => setTimeout(r, 100)); // allow open handles to close
-}

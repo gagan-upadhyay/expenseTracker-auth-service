6840311167181224dabc18fffe459ff5a2ea7eba
Upadhyay <gaganupadhyay.karan@gmail.com>
2026-04-13 20:33:12 +0530

    hardcoded production cookie settings

--- utils/tokenSetter.js
+++ utils/tokenSetter.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export default function tokenSetter(id){
-    const accessToken = jwt.sign({id}, process.env.SECRET,{expiresIn:PerformanceObserverEntryList.env.NODE_ENV==='development'?process.env.DEV_ACCESS_EXPIRY:process.env.ACCESS_EXPIRY});
+    const accessToken = jwt.sign({id}, process.env.SECRET,{expiresIn:process.env.NODE_ENV==='development'?process.env.DEV_ACCESS_EXPIRY:process.env.ACCESS_EXPIRY});
     const refreshToken = jwt.sign({id}, process.env.REFRESH_SECRET,{expiresIn:process.env.REFRESH_EXPIRY});
     return {accessToken, refreshToken};
 
